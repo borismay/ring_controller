9ee5237f8da566c93bb4af0e654f408e26fe0ac2
borismay <borismay@gmail.com>
2021-03-02 11:34:17 -0800

    split to functions

--- ring_controller.py
+++ ring_controller.py
@@ -52,19 +52,6 @@ def ping_all_units(ips):
     return replies
 
 
-def tests():
-    # ips = ['192.168.1.33', '142.250.72.206']
-    # reply = ping(TEST_IP, count=PING_PACKETS, timeout=PING_TIMEOUT_SEC, size=PING_PACKET_SIZE_BYTES)
-    # print(f'RTT ms: {reply.rtt_avg_ms}')
-    # print(f'Packet loss: {reply.packet_loss}')
-    #
-    # u0 = UnitPing(ips[0])
-    # u0.is_reachable()
-    # u1 = UnitPing(ips[1])
-    # u1.is_reachable()
-    return
-
-
 def find_first_unconnected_ip(unconnected_ips, all_ips_ordered):
     for ip in all_ips_ordered:
         if ip in unconnected_ips:
@@ -74,6 +61,25 @@ def find_first_unconnected_ip(unconnected_ips, all_ips_ordered):
 def find_last_connected_ip(unconnected_ip, all_ips_ordered):
     last_connected_index = all_ips_ordered.index(unconnected_ip) - 1
     return all_ips_ordered[last_connected_index]
+
+
+def activate_rpl(unit):
+    rpl_ip = unit['IP'].values[0]
+    print(f'Activating RPL of {rpl_ip}')
+    return
+
+
+def disconnect_last_connected_unit(unit):
+    # check the connection type of the last connected unit
+    connection_to_the_next_unit = unit['ConnectionToNextRadio'].values[0]
+    last_connected_ip = unit['IP'].values[0]
+
+    if connection_to_the_next_unit == 'rf':
+        print(f'Connection to the next unit is RF. Need to turn on ALIGNMENT of {last_connected_ip}')
+    else:
+        print(
+            f'Connection to the next unit is Ethernet. Need to turn down {connection_to_the_next_unit} of {last_connected_ip}')
+    return
 
 
 if __name__ == "__main__":
@@ -90,6 +96,9 @@ if __name__ == "__main__":
 
         if all_alive:
             print('All units alive.')
+            print(f'Sleeping for {DELAY_BETWEEN_PING_TEST_SEC} seconds.')
+            time.sleep(DELAY_BETWEEN_PING_TEST_SEC)
+            continue
 
         else:
             print('At least one unit is disconnected!')
@@ -124,16 +133,24 @@ if __name__ == "__main__":
             print(f'Unconnected direction: {unconnected_direction}')
             print(f'First unconnected IP: {first_unconnected_ip}')
 
-            # check the connection type of the last connected unit
-            connection_to_the_next_unit = units_in_ring[units_in_ring["IP"] == last_connected_ip]['ConnectionToNextRadio'].values[0]
-            if connection_to_the_next_unit == 'rf':
-                print(f'Connection to the next unit is RF. Need to turn on ALIGNMENT of {last_connected_ip}')
-            else:
-                print(f'Connection to the next unit is Ethernet. Need to turn down {connection_to_the_next_unit} of {last_connected_ip}')
+            # disconnect the last connected unit
+            disconnect_last_connected_unit(units_in_ring[units_in_ring["IP"] == last_connected_ip])
 
+            # activate RPL
+            # if the disconnect is on the ACW side, the RPL should be activated on the CW side
+            if unconnected_direction == 'ACW':
+                rpl_side = 'CW'
+            else:
+                rpl_side = 'ACW'
+
+            rpl_activation_unit = units_in_ring[
+                (units_in_ring["ConnectionToNextRadio"] == 'rpl') &
+                (units_in_ring["Direction"] == rpl_side)
+            ]
+
+            activate_rpl(rpl_activation_unit)
+
+            # exit if a fault was detected
             if EXIT_ON_FAULT:
                 print('Exiting...')
                 break
-
-        print(f'Sleeping for {DELAY_BETWEEN_PING_TEST_SEC} seconds.')
-        time.sleep(DELAY_BETWEEN_PING_TEST_SEC)
